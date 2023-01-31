7b786df3df268977f3460fc5f2e5c7a3e299d129
Antony Cochet <cochetantony@gmail.com>
2023-01-31 10:46:24 +0100

    tableau de bord

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,7 +7,7 @@ export default function dashboard() {
       <header>
         <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Dashboard
+            Tableau de bord
           </h1>
         </div>
       </header>
